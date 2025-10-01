6648b378854fa05b0e2122565468094f3ae11eb0
shravakInnogent <shravak.jain@innogent.in>
2025-10-01 18:34:33 +0530

    Delete Innogent/com/com directory

--- Innogent/com/com/company/InvalidAgeException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-public class InvalidAgeException extends Exception {
-
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
--- Innogent/com/com/company/InvalidMarksException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class InvalidMarksException extends Exception{
-    InvalidMarksException(String message){
-        super(message);
-    }
-}
